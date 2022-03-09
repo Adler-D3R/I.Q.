abd4669a690fa61ba822a6c775698e322c5a35a6
Adler <57773679+Adler-D3R@users.noreply.github.com>
2022-03-09 16:24:57 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -203,7 +203,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int isdigit_str(char *str)
+int CheckIfIsDigitString(char* str)
 {
 	for (int i = 0; str[i] != '\0'; i++)
 		if (str[i] < '0' || str[i] > '9') return 0;
