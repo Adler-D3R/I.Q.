d7a11da34ee98bbfa3d1a3a7b3b97a4e067a7869
Adler <57773679+Adler-D3R@users.noreply.github.com>
2022-03-09 15:25:52 +0100

    Update F_Scan.h

--- Headers/F_Scan.h
+++ Headers/F_Scan.h
@@ -1,9 +1,9 @@
 #ifndef F_SCAN_HEADER
 #define F_SCAN_HEADER
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
